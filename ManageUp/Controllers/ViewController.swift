d3754be61bad9fcabb404b26c6ecb96e710ff469
Ariel Higuera <purpl3m00ns@gmail.com>
2023-07-21 13:27:15 -0700

    Add basic file structure

---
+++ ManageUp/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManageUp
+//
+//  Created by Ariel Higuera on 7/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
