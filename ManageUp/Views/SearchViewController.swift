736164d2bd089363a6f2284373ef94f5e8958b55
Ariel Higuera <purpl3m00ns@gmail.com>
2023-07-25 15:12:07 -0700

    Add basic layout for Search View

---
+++ ManageUp/Views/SearchViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SearchViewController.swift
+//  ManageUp
+//
+//  Created by Ariel Higuera on 7/25/23.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logoutPressed(_ sender: UIBarButtonItem) {
+        do {
+        try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
