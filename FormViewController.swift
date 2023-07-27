19347b9587a034052ce851868b1b0961e24339fd
Ariel Higuera <purpl3m00ns@gmail.com>
2023-07-27 13:25:01 -0700

    Add tags via UI, see them displayed, and save to database as part of the entry

--- FormViewController.swift
+++ FormViewController.swift
@@ -13,17 +13,24 @@ class FormViewController: UIViewController {
 
     
     @IBOutlet weak var entryText: UITextView!
+    @IBOutlet weak var tagEntryField: UITextField!
+    @IBOutlet weak var tagTableView: UITableView!
     
     let db = Firestore.firestore()
     
+    var tags: [String] = ["success", "or not?"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tagTableView.delegate = self
+        tagTableView.dataSource = self
 
     }
+
     
     @IBAction func submitPressed(_ sender: UIButton) {
         if let textBody = entryText.text, let user = Auth.auth().currentUser?.email {
-            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.textField: textBody, K.FStore.dateField: Date.now, K.FStore.userField: user, K.FStore.tagsField: ["Example Tag"]]) { (error) in
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.textField: textBody, K.FStore.dateField: Date.now, K.FStore.userField: user, K.FStore.tagsField: tags]) { (error) in
                 if let e = error {
                     print(e)
                 } else {
@@ -32,6 +39,15 @@ class FormViewController: UIViewController {
             }
         }
         print(entryText.text!)
+    }
+    
+    
+    @IBAction func addTagPressed(_ sender: UIButton) {
+        if let newTag = tagEntryField.text {
+            tags.append(newTag)
+            tagEntryField.text = ""
+        }
+        tagTableView.reloadData()
     }
     
     @IBAction func logoutPressed(_ sender: UIBarButtonItem) {
@@ -43,4 +59,24 @@ class FormViewController: UIViewController {
         }
     }
 
+}
+
+//populates table view
+extension FormViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tags.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableTagCell")
+        cell?.textLabel?.text = tags[indexPath.row]
+        return cell!
+    }
+}
+
+extension FormViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // TODO: this function is where I can tell it what to do when the user clicks on an entry listed in the table
+        print(indexPath.row)
+    }
 }
