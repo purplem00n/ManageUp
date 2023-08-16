8bc8c2798e5de457ed50403a0770338344b987f4
Ariel Higuera <purpl3m00ns@gmail.com>
2023-08-16 14:14:41 -0700

    Edge alignment adjustments

---
+++ ManageUp/Models/Mail.swift
@@ -0,0 +1,16 @@
+//
+//  Mail.swift
+//  ManageUp
+//
+//  Created by Ariel Higuera on 8/13/23.
+//
+
+import Foundation
+import UIKit
+
+struct Mail {
+    let to: [String]
+    let message: [String: String]
+    let subject: String
+    let text: String
+}
